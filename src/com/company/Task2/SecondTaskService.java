156bd38ca06124f6aa3d04008daaaada0087af39
Vitalik Drapak <vitalik.drapak@redtag.pro>
2021-12-03 10:17:10 +0200

    Added try-with-resources

--- src/com/company/Task2/SecondTaskService.java
+++ src/com/company/Task2/SecondTaskService.java
@@ -4,8 +4,7 @@ import java.util.Scanner;
 
 public class SecondTaskService {
     public static void createUser(){
-        Scanner sc = new Scanner(System.in);
-        try {
+        try (Scanner sc = new Scanner(System.in)) {
             System.out.print("FirstName: ");
             String firstName = sc.nextLine();
             System.out.print("LastName: ");
@@ -16,11 +15,9 @@ public class SecondTaskService {
             int age = Integer.parseInt(sc.nextLine());
 
             User user = new User(firstName, lastName, email, age, new Address());
-        }catch (IllegalParamsException e){
+        } catch (IllegalParamsException e) {
             System.out.println("Exception message: " + e.getMessage());
             System.out.println("Exception code: " + e.getCode().toString());
-        }finally {
-            sc.close();
         }
     }
 }
